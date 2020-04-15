2af396816ec3bf17c89cc4d258f3e144f6f5bf36
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2020-04-15 22:56:41 +0200

    Implemented EXCLUDE_LOG_TIME env variable setting

--- apartment_notifier/settings.py
+++ apartment_notifier/settings.py
@@ -1,21 +1,26 @@
 import os
 
+_TRUE_LIST = ('1', 'true', 'yes', 'y')
 store = os.environ.get('STORE', 'store.json')
 telegram_api_key = os.environ['TELEGRAM_API_KEY']
-print_notifier = os.environ.get('PRINT_NOTIFIER', 'false').lower() in ('1', 'true')
+print_notifier = os.environ.get('PRINT_NOTIFIER', 'false').lower() in _TRUE_LIST
+exclude_log_time = os.environ.get('EXCLUDE_LOG_TIME', 'false').lower() in _TRUE_LIST
 
 
 LOGGING = {
     'version': 1,
     'formatters': {
         'verbose': {
-            'format': '%(asctime)s [%(name)s:%(lineno)-3d] %(levelname)-7s: %(message)s'
-        }
+            'format': '%(asctime)s [%(name)s:%(lineno)-3d] %(levelname)-7s: %(message)s',
+        },
+        'verbose_without_time': {
+            'format': '[%(name)s:%(lineno)-3d] %(levelname)-7s: %(message)s',
+        },
     },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
+            'formatter': 'verbose_without_time' if exclude_log_time else 'verbose',
             # 'stream': 'ext://sys.stdout',
         },
     },
