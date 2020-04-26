69678ed1f1380bc33636c4a8c55a8335a76453c4
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2020-04-26 18:20:29 +0200

    Need to specify google project id to deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,13 @@ if [[ ! -f "$PROD_ENV_FILE" ]]; then
     exit 1
 fi
 export $(cat "$PROD_ENV_FILE")
-echo ${PATH}
+
+if [[ -z "${PROJECT_ID}" ]]; then
+    echo "You need to specify PROJECT_ID"
+    exit 1
+fi
+
+gcloud config set project "${PROJECT_ID}"
 envsubst < app.templ.yaml > app.yaml
 gcloud --quiet app deploy app.yaml
 gcloud --quiet app deploy cron.yaml
